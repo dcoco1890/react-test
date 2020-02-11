7628285b664dad5f86a2d9bdfbaa63a10bea4779
Cohen <dcoco91@gmail.com>
2020-02-11 10:09:14 -0500

    Updates

---
+++ precache-manifest.0a5d6ca113112d0a9a7c608005836ff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c8b5ca9b71204edbae9",
+    "url": "/react-test/static/css/main.7a236633.chunk.css"
+  },
+  {
+    "revision": "9c8b5ca9b71204edbae9",
+    "url": "/react-test/static/js/main.d42ec537.chunk.js"
+  },
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  },
+  {
+    "revision": "cd942d3339e65cc3ca1b",
+    "url": "/react-test/static/js/2.18baa640.chunk.js"
+  },
+  {
+    "revision": "cea7f883991914150b5c122f8e108498",
+    "url": "/react-test/index.html"
+  }
+];
